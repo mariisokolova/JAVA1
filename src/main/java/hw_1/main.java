534d4a7b762256e5d4e4701f8d902b621ed7006e
Deermeapart <deermeapart@gmail.com>
2020-10-07 21:31:32 +0300

    Homework lesson 2

--- src/main/java/hw_1/main.java
+++ src/main/java/hw_1/main.java
@@ -5,73 +5,89 @@ import java.util.Arrays;
 public class main {
 
     public static void main(String[] args) {
-        // 1. Задать целочисленный массив, состоящий из элементов 0 и 1.
-        // Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
-        // С помощью цикла и условия заменить 0 на 1, 1 на 0;
-        int[] task1 = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-        for (int i = 0; i < task1.length; i++) {
-            if (task1[i] == 1){
-                task1[i] = 0;
-            }
-            else {
-                task1[i] = 1;
-            }
+        System.out.println(isLeapYear(2400));
+    }
+
+    // 2. Создать переменные всех пройденных типов данных, и инициализировать их значения;
+    byte b1 = 10;               // -128 127
+    short s1 = 32766;           // -32768 32767
+    int i1 = -3333333;          // от -2147483648 2147483647
+    long l1 = 334343243232L;     // от -9223372036854775808 9223372036854775807
+    float f1 = 23.34f;
+    double d1 = 32.23;
+    char c1 = 'd';
+    boolean bl = true;
+    String str = "Hello, world";
+
+
+    // 3. Написать метод вычисляющий выражение a * (b + (c / d))
+    // и возвращающий результат,где a, b, c, d – входные параметры этого метода;
+    public static int count(int a, int b, int c, int d) {
+        int calc = a * (b + (c / d));
+        return calc;
+    }
+
+
+    // 4. Написать метод, принимающий на вход два числа,
+    // и проверяющий что их сумма лежит в пределах от 10 до 20(включительно),
+    // если да – вернуть true, в противном случае – false;
+    public static boolean checkIntBetween(int a, int b) {
+        if ((a + b) > 10 && (a + b ) < 20) {
+            return true;
         }
-        System.out.println("Задание 1: " + Arrays.toString(task1));
-
-
-        // 2. Задать пустой целочисленный массив размером 8.
-        // С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
-        int[] task2 = new int[8];
-        for (int i = 0, j = 0; j < task2.length; i+=3, j += 1) {
-            task2[j] = i;
+        else {
+            return false;
         }
-        System.out.println("Задание 2: " + Arrays.toString(task2));
+    }
 
 
-        // 3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ]
-        // пройти по нему циклом, и числа меньшие 6 умножить на 2;
-        int[] task3 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        for (int i = 0; i < task3.length; i++) {
-            if (task3[i] < 6) {
-                task3[i] *= 2;
-            }
+    // 5. Написать метод, которому в качестве параметра передается целое число,
+    // метод должен напечатать в консоль положительное ли число передали, или отрицательное;
+    // Замечание: ноль считаем положительным числом.
+    public static String checkIntPositive(int a){
+        if (a >= 0) {
+            String result = "Число " + a + " положительное";
+            return result;
         }
-        System.out.println("Задание 3: " + Arrays.toString(task3));
-
-
-        // 4. Создать квадратный двумерный целочисленный массив
-        // (количество строк и столбцов одинаковое),
-        // и с помощью цикла(-ов) заполнить его диагональные элементы единицами;
-        int[][] table = new int[5][5];
-        System.out.println("Задание 4:");
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                if (i == j){
-                    table[i][j] = 1;
-                }
-                System.out.print(table[i][j]);
-            }
-            System.out.println();
+        else {
+            String result = "Число " + a + " отрицательное";
+            return result;
         }
+    }
 
 
-        // 5. ** Задать одномерный массив и
-        // найти в нем минимальный и максимальный элементы (без помощи интернета);
-        int[] task5 = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        for (int i = task5.length - 1; i > 0; i--) {
-            for (int j = 0; j < i; j++) {
-                if (task5[j] > task5[j + 1]) {
-                    int tmp = task5[j];
-                    task5[j] = task5[j + 1];
-                    task5[j + 1] = tmp;
-                }
-            }
+    // 6. Написать метод, которому в качестве параметра передается целое число,
+    // метод должен вернуть true, если число отрицательное;
+    public static boolean isNegativeInt(int a) {
+        if (a < 0) {
+            return true;
         }
-        System.out.println("Задание 5");
-        System.out.println("Самое маленькое число: " + task5[0]);
-        System.out.println("Самое большое число: " + task5[task5.length - 1]);
+        else {
+            return false;
+        }
+    }
 
 
+    //7. Написать метод, которому в качестве параметра передается строка, обозначающая имя,
+    // метод должен вывести в консоль сообщение «Привет, указанное_имя!»;
+    public static String printName(String name) {
+        String greeting = "Привет, " + name;
+        return greeting;
+    }
+
+
+    //8. * Написать метод, который определяет является ли год високосным, и выводит сообщение в консоль.
+    // Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
+
+    public static String isLeapYear(int a) {
+        if ((a % 4) == 0 &&
+                ((a % 100) != 0 || (a % 400) == 0)) {
+
+            String result = "Год " + a + " - високосный";
+            return result;
+        } else {
+            String result = "Год " + a + " - не високосный";
+            return result;
+        }
     }
     }
